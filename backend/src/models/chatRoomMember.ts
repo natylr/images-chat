ad6e9de9d93821e3b2bb41f73bfdc691b0f7ff68
natylr <51606937+natylr@users.noreply.github.com>
2024-07-22 17:06:46 +0300

    Add check function if userID exists

--- backend/src/models/chatRoomMember.ts
+++ backend/src/models/chatRoomMember.ts
@@ -1,4 +1,5 @@
 import { Schema, Document, model, Types } from 'mongoose';
+import { checkUserIdExists } from '../middleware/checkUserIdExistsMiddleware';
 
 export interface IChatRoomMember extends Document {
   userID: Types.ObjectId;
@@ -12,4 +13,5 @@ const chatRoomMemberSchema = new Schema<IChatRoomMember>({
   joinedAt: { type: Date, default: Date.now }
 });
 
+chatRoomMemberSchema.pre('save', checkUserIdExists)
 export const ChatRoomMember = model<IChatRoomMember>('ChatRoomMember', chatRoomMemberSchema);
