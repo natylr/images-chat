7cdc7015a41cb58b48378cb2eeb3e84b86d8dc37
natylr <51606937+natylr@users.noreply.github.com>
2024-07-29 21:42:39 +0300

    Add checkChatRoomIdExists function to verify user existence

---
+++ backend/src/utils/checkChatRoomIdExists.ts
@@ -0,0 +1,6 @@
+import { ChatRoom } from "../models/chatRoom";
+
+export const checkChatRoomIdExists = async (chatRoom: string): Promise<boolean> => {
+  const user = await ChatRoom.findById(chatRoom);
+  return !!user;
+};
