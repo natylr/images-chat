ad948265d7e80941d045450ef1b92abeb06b6051
natylr <51606937+natylr@users.noreply.github.com>
2024-07-31 18:17:10 +0300

    Add ChatRoomIdExistsMiddleware.ts to validate chat room ID existence before proceeding with request

---
+++ backend/src/middleware/checkChatRoomIdExistsMiddleware.ts
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+import { Document, Types } from 'mongoose';
+import {checkChatRoomIdExists} from '../utils/checkChatRoomIdExists';
+
+export const checkChatRoomIdExistsMiddleware = async function(this:Document, next: Function)  {
+  try {
+    const userId = this._id as Types.ObjectId;
+    const user = await checkChatRoomIdExists(userId.toString());
+
+    if (!user) {
+      throw new mongoose.Error('Invalid ChatRoom Association: ChatRoom with the provided chatRoomID does not exist');
+    }
+
+    next(); 
+  } catch (error) {
+    next(error); 
+  }
+};
