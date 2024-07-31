f1a50679d83562e9b3760efe187b88fdd62a30c9
natylr <51606937+natylr@users.noreply.github.com>
2024-07-31 16:28:00 +0300

    Use checkUserIdExists function in checkUserIdExistsMiddleware

--- backend/src/middleware/checkUserIdExistsMiddleware.ts
+++ backend/src/middleware/checkUserIdExistsMiddleware.ts
@@ -1,12 +1,11 @@
 import mongoose from "mongoose";
 import { Document, Types } from 'mongoose';
+import {checkUserIdExists} from '../utils/checkUserIdExists';
 
-import { User } from "../models/user";
-
-export const checkUserIdExists = async function(this:Document, next: Function)  {
+export const checkUserIdExistsMiddleware = async function(this:Document, next: Function)  {
   try {
     const userId = this._id as Types.ObjectId;
-    const user = await User.findById(userId);
+    const user = await checkUserIdExists(userId.toString());
 
     if (!user) {
       throw new mongoose.Error('Invalid User Association: User with the provided userID does not exist');
