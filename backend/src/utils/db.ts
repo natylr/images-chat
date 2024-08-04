b8c51c50487163e47c20a014a70ae53d99ecd657
natylr <51606937+natylr@users.noreply.github.com>
2024-08-04 21:37:17 +0300

    Created `db.ts` utility to manage database connections.

---
+++ backend/src/utils/db.ts
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose';
+import { mongoUrl } from '../secret';
+
+export const connectToDatabase = async () => {
+  try {
+    await mongoose.connect(mongoUrl);
+    console.log("Connected to database");
+  } catch (error) {
+    console.error('Error connecting to the database', error);
+    process.exit(1);
+  }
+};
+
+export const disconnectFromDatabase = async () => {
+  try {
+    await mongoose.disconnect();
+    console.log('Disconnected from the database');
+  } catch (error) {
+    console.error('Error disconnecting from the database', error);
+  }
+};
