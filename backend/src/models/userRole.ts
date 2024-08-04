8001740d6178f498853b0b5bbbcd49da77c1c2fb
natylr <51606937+natylr@users.noreply.github.com>
2024-08-04 19:21:39 +0300

    Updated `userRole.ts` to reference the Role model.

--- backend/src/models/userRole.ts
+++ backend/src/models/userRole.ts
@@ -3,14 +3,14 @@ import { IUserChatRoomReference } from '../interfaces/IUserChatRoomReference ';
 import { checkUserAndChatRoomId } from '../middleware/checkUserAndChatRoomId';
 
 export interface IUserRole extends Document, IUserChatRoomReference {
-  role: string;
+  role: Types.ObjectId;
   assignedAt: Date;
 }
 
 const userRoleSchema = new Schema<IUserRole>({
   userID: { type: Schema.Types.ObjectId, ref: 'User', required: true },
   chatRoomID: { type: Schema.Types.ObjectId, ref: 'ChatRoom', required: true },
-  role: { type: String, required: true },
+  role: { type: Schema.Types.ObjectId, ref: 'Role', required: true },
   assignedAt: { type: Date, default: Date.now }
 });
 userRoleSchema.pre('save', checkUserAndChatRoomId)
