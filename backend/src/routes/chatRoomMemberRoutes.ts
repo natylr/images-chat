420fc967e7aa9fdd85d6bc89df1f56b7d8934e6b
natylr <51606937+natylr@users.noreply.github.com>
2024-08-16 18:39:08 +0300

    Enhance chatRoomMemberRoutes to include permission checks and JWT verification for chat room member operations

--- backend/src/routes/chatRoomMemberRoutes.ts
+++ backend/src/routes/chatRoomMemberRoutes.ts
@@ -1,9 +1,8 @@
-// chatRoomMemberRoutes.ts 
 import { Router } from 'express';
 import { checkPermissions } from '../middleware/checkPermissions';
 import { createChatRoomMember, getAllChatRoomMembers, getChatRoomMemberById, updateChatRoomMemberById, deleteChatRoomMemberById, leaveChatRoom } from '../controllers/chatRoomMemberController';
 import verifyJWT from '../middleware/verifyJWT';
-
+import verifyUserOwnsResource from '../middleware/verifyUserOwnsResource'
 const router: Router = Router();
 
 router.post('/chatroommembers',verifyJWT, createChatRoomMember);
@@ -11,6 +10,6 @@ router.get('/chatroommembers',verifyJWT, getAllChatRoomMembers);
 router.get('/chatroommembers/:id',verifyJWT, getChatRoomMemberById);
 router.put('/chatroommembers/:id',verifyJWT, updateChatRoomMemberById);
 router.delete('/chatroommembers/:id',verifyJWT, deleteChatRoomMemberById);
-router.post('/leaveChatRoom',verifyJWT, checkPermissions('LEAVE_CHAT_ROOM'), leaveChatRoom);
+router.post('/leaveChatRoom', verifyJWT, checkPermissions('LEAVE_CHAT_ROOM', verifyUserOwnsResource), leaveChatRoom);
 
 export default router;
