875dafe34cb47d7e15b5223e242702e585a7c90c
natylr <51606937+natylr@users.noreply.github.com>
2024-08-18 00:11:28 +0300

    Update message routes permissions and endpoint

--- backend/src/routes/messageRoutes.ts
+++ backend/src/routes/messageRoutes.ts
@@ -1,13 +1,15 @@
 import { Router } from 'express';
 import { createMessage, getAllMessages, getMessageById, updateMessageById, deleteMessageById } from '../controllers/messageController';
 import verifyJWT from '../middleware/verifyJWT';
+import { checkPermissions } from '../middleware/checkPermissions';
+import verifyUserOwnsResource from '../middleware/verifyUserOwnsResource';
 
 const router: Router = Router();
 
-router.post('/messages',verifyJWT, createMessage);
+router.post('/messages',checkPermissions('CREATE_MESSAGE'), verifyJWT, createMessage);
 router.get('/messages',verifyJWT, getAllMessages);
 router.get('/messages/:id',verifyJWT, getMessageById);
-router.put('/messages/:id',verifyJWT, updateMessageById);
-router.delete('/messages/:id',verifyJWT, deleteMessageById);
+router.put('/messages/:id',checkPermissions('EDIT_MESSAGE', verifyUserOwnsResource) ,verifyJWT, updateMessageById);
+router.delete('/messages/:id',checkPermissions('DELETE_MESSAGE'),verifyJWT, deleteMessageById);
 
 export default router;
