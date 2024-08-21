bab5a1c1e37380f27beb93093563b1b63f68f3fb
natylr <51606937+natylr@users.noreply.github.com>
2024-08-21 18:27:45 +0300

    Reordered message update permission check
    - Reordered the middleware chain in the PUT /messages/:id route to perform JWT verification before permission check. This ensures a user has a valid JWT before verifying their permissions to update a message.

--- backend/src/routes/messageRoutes.ts
+++ backend/src/routes/messageRoutes.ts
@@ -6,10 +6,10 @@ import verifyUserOwnsResource from '../middleware/verifyUserOwnsResource';
 
 const router: Router = Router();
 
-router.post('/messages',checkPermissions('CREATE_MESSAGE'), verifyJWT, createMessage);
-router.get('/messages',verifyJWT, getAllMessages);
-router.get('/messages/:id',verifyJWT, getMessageById);
-router.put('/messages/:id',checkPermissions('EDIT_MESSAGE', verifyUserOwnsResource) ,verifyJWT, updateMessageById);
-router.delete('/messages/:id',checkPermissions('DELETE_MESSAGE'),verifyJWT, deleteMessageById);
+router.post('/messages', checkPermissions('CREATE_MESSAGE'), verifyJWT, createMessage);
+router.get('/messages', verifyJWT, getAllMessages);
+router.get('/messages/:id', verifyJWT, getMessageById);
+router.put('/messages/:id', verifyJWT, checkPermissions('EDIT_MESSAGE', verifyUserOwnsResource), updateMessageById);
+router.delete('/messages/:id', verifyJWT, checkPermissions('DELETE_MESSAGE'), deleteMessageById);
 
 export default router;
