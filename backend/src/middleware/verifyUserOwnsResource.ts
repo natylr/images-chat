f5bf4a0bac991e798ee5d10f0bd7a428f11aa571
natylr <51606937+natylr@users.noreply.github.com>
2024-09-02 16:52:09 +0300

    Refactor verifyUserOwnsResource to check for body and userID

--- backend/src/middleware/verifyUserOwnsResource.ts
+++ backend/src/middleware/verifyUserOwnsResource.ts
@@ -2,9 +2,14 @@ import { IUserIdRequest } from '../interfaces/IUserIdRequest';
 import { Types } from 'mongoose';
 
 const verifyUserOwnsResource = (req: IUserIdRequest): boolean => {
-  const userIdFromToken = req.userId; // This is set by the verifyJWT middleware
-  const userIdFromBody = req.body.userID; // This is the userID in the request body
-  return userIdFromToken === userIdFromBody;
+  if ("body" in req && "userID" in req.body){
+    const userIdFromToken = req.userId; // This is set by the verifyJWT middleware
+    const userIdFromBody = req.body.userID; // This is the userID in the request body
+    return userIdFromToken === userIdFromBody;
+  }
+  else{
+    return false
+  }
 };
 
 export default verifyUserOwnsResource;
