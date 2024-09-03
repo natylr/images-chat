2f7caa09dc7a6af8709469504e5dfa39157a8f82
natylr <51606937+natylr@users.noreply.github.com>
2024-09-03 21:37:47 +0300

    Replace import of bcryptjs

--- backend/src/utils/validatePassword.ts
+++ backend/src/utils/validatePassword.ts
@@ -1,4 +1,4 @@
-import bcrypt from 'bcryptjs';
+const bcrypt = require('bcryptjs')
 
 export const validatePassword = async (providedPassword: string, storedHashedPassword: string): Promise<boolean> => {
   return await bcrypt.compare(providedPassword, storedHashedPassword);
