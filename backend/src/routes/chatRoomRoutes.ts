e8248558664babef345034a5fd3520cc5a2a4a41
natylr <51606937+natylr@users.noreply.github.com>
2024-09-03 18:19:56 +0300

    Added permission check for UPDATE_CHATROOM

--- backend/src/routes/chatRoomRoutes.ts
+++ backend/src/routes/chatRoomRoutes.ts
@@ -7,7 +7,7 @@ const router: Router = Router();
 router.post('/chatrooms',verifyJWT, createChatRoom);
 router.get('/chatrooms',verifyJWT, getAllChatRooms);
 router.get('/chatrooms/:id',verifyJWT, getChatRoomById);
-router.put('/chatrooms/:id',verifyJWT, updateChatRoomById);
+router.put('/chatrooms/:id',verifyJWT, checkPermissions("UPDATE_CHATROOM"), updateChatRoomById);
 router.delete('/chatrooms/:id',verifyJWT, checkPermissions("DELETE_CHATROOM"), deleteChatRoomById);
 
 export default router;
