09440ce7ca2a855bc273fd0182ea606f61671c94
natylr <51606937+natylr@users.noreply.github.com>
2024-09-03 17:06:33 +0300

    Added permission check for DELETE_CHATROOM

--- backend/src/routes/chatRoomRoutes.ts
+++ backend/src/routes/chatRoomRoutes.ts
@@ -1,13 +1,14 @@
 import { Router } from 'express';
 import { createChatRoom, getAllChatRooms, getChatRoomById, updateChatRoomById, deleteChatRoomById } from '../controllers/chatRoomController'
 import verifyJWT from '../middleware/verifyJWT';
+import { checkPermissions } from '../middleware/checkPermissions';
 const router: Router = Router();
 
 router.post('/chatrooms',verifyJWT, createChatRoom);
 router.get('/chatrooms',verifyJWT, getAllChatRooms);
 router.get('/chatrooms/:id',verifyJWT, getChatRoomById);
 router.put('/chatrooms/:id',verifyJWT, updateChatRoomById);
-router.delete('/chatrooms/:id',verifyJWT, deleteChatRoomById);
+router.delete('/chatrooms/:id',verifyJWT, checkPermissions("DELETE_CHATROOM"), deleteChatRoomById);
 
 export default router;
 
