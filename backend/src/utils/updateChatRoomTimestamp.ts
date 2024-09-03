177ecdc5a791d4e4d136ada14000880cc9fc0463
natylr <51606937+natylr@users.noreply.github.com>
2024-09-03 21:24:20 +0300

    Add updateChatRoomTimestamp util for update updateAt field

---
+++ backend/src/utils/updateChatRoomTimestamp.ts
@@ -0,0 +1,6 @@
+import { Types } from 'mongoose';
+import { ChatRoom } from '../models/chatRoom';
+
+export async function updateChatRoomTimestamp(chatRoomID: Types.ObjectId): Promise<void> {
+  await ChatRoom.findByIdAndUpdate(chatRoomID, { updatedAt: new Date() });
+}
