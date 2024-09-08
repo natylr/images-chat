13d2e75ab7f0f9cdfb7682f6c462262368269aaa
natylr <51606937+natylr@users.noreply.github.com>
2024-09-08 16:54:30 +0300

    feat: Add category field to image schema

--- backend/src/models/image.ts
+++ backend/src/models/image.ts
@@ -1,12 +1,14 @@
-import { Schema, Document, model} from "mongoose";
+import { Schema, Document, model, Types} from "mongoose";
 
 export interface IImage extends Document{
     URL: string;
+    category: Types.ObjectId;
     createdAt: Date;
 }
 
 const imageSchema = new Schema<IImage> ({
     URL: {type: String, required:true, unique:true},
+    category:{ type: Schema.Types.ObjectId, require:true, ref: 'Category' },
     createdAt: { type: Date, default: Date.now, required: true}
 })
 
