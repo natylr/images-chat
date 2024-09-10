72fabb264794a82f9887fe608c2277cc44761760
natylr <51606937+natylr@users.noreply.github.com>
2024-09-10 16:23:49 +0300

    Refactor ChatRoomMember controller - Remove redundant update functionality This commit removes the updateChatRoomMemberById function from the ChatRoomMember controller. The rationale behind this change is that updating a chat room member's information is likely less common than creating or deleting a member entirely. Additionally, the comment within the code suggests there's no intention to support updates in the future.
    This refactoring simplifies the controller and reduces potential code complexity.

--- backend/src/routes/chatRoomMemberRoutes.ts
+++ backend/src/routes/chatRoomMemberRoutes.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import { checkPermissions } from '../middleware/checkPermissions';
-import { createChatRoomMember, getAllChatRoomMembers, getChatRoomMemberById, updateChatRoomMemberById, deleteChatRoomMemberById, leaveChatRoom } from '../controllers/chatRoomMemberController';
+import { createChatRoomMember, getAllChatRoomMembers, getChatRoomMemberById, deleteChatRoomMemberById, leaveChatRoom } from '../controllers/chatRoomMemberController';
 import verifyJWT from '../middleware/verifyJWT';
 import verifyUserOwnsResource from '../middleware/verifyUserOwnsResource'
 const router: Router = Router();
@@ -8,7 +8,6 @@ const router: Router = Router();
 router.post('/chatroommembers', verifyJWT, createChatRoomMember);
 router.get('/chatroommembers', verifyJWT, getAllChatRoomMembers);
 router.get('/chatroommembers/:id', verifyJWT, getChatRoomMemberById);
-router.put('/chatroommembers/:id', verifyJWT, checkPermissions('ADD_MEMBER'), updateChatRoomMemberById);
 router.delete('/chatroommembers/:id', verifyJWT, checkPermissions('REMOVE_MEMBER'), deleteChatRoomMemberById);
 router.post('/leaveChatRoom', verifyJWT, checkPermissions('LEAVE_CHAT_ROOM', verifyUserOwnsResource), leaveChatRoom);
 
