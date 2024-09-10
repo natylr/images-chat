b61b506b0f59a7fd54214b5e0772b3631df24d3b
natylr <51606937+natylr@users.noreply.github.com>
2024-09-10 16:43:45 +0300

    Simplify Category controller - Remove update functionality This commit removes the updateCategory function from the Category controller.
    This simplification improves code maintainability and reduces the attack surface for potential errors.

--- backend/src/controllers/categoryController.ts
+++ backend/src/controllers/categoryController.ts
@@ -40,28 +40,6 @@ export const getCategoryById = async (req: Request, res: Response) => {
     }
 };
 
-// Update a category
-export const updateCategory = async (req: Request, res: Response) => {
-    try {
-        const { name } = req.body;
-        const updatedCategory = await Category.findByIdAndUpdate(
-            req.params.id,
-            { name },
-            { new: true, runValidators: true }
-        );
-        if (!updatedCategory) {
-            return res.status(404).json({ message: 'Category not found' });
-        }
-        res.status(200).json(updatedCategory);
-    } catch (error) {
-        if (error.code === 11000) { // Duplicate key error
-            res.status(400).json({ message: 'Category name must be unique' });
-        } else {
-            res.status(500).json({ message: 'Server Error', error });
-        }
-    }
-};
-
 // Delete a category
 export const deleteCategory = async (req: Request, res: Response) => {
     try {
--- backend/src/routes/categoryRoutes.ts
+++ backend/src/routes/categoryRoutes.ts
@@ -1,12 +1,11 @@
 import { Router } from 'express';
-import { createCategory, getCategories, getCategoryById, updateCategory, deleteCategory} from '../controllers/categoryController';
+import { createCategory, getCategories, getCategoryById, deleteCategory} from '../controllers/categoryController';
 
 const router = Router();
 
 router.post('/categories', createCategory);
 router.get('/categories', getCategories);
 router.get('/categories/:id', getCategoryById);
-router.put('/categories/:id', updateCategory);
 router.delete('/categories/:id', deleteCategory);
 
 export default router;
