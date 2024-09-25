b6e4171e06c522fc75fcc96d2d55f08fdabcb331
natylr <51606937+natylr@users.noreply.github.com>
2024-09-25 12:45:35 +0300

    refactor: use checkExistsInUser for userId validation in UserChatRoomValidator instead checkUserIdExists

--- backend/src/services/userChatRoomValidator.ts
+++ backend/src/services/userChatRoomValidator.ts
@@ -1,11 +1,12 @@
 import { checkChatRoomIdExists } from '../utils/validation/checkChatRoomIdExists';
+import { checkExistsInUser } from '../utils/validation/checkExistsInUser';
 import { checkUserIdExists } from '../utils/validation/checkUserIdExists';
 import mongoose, { Types } from 'mongoose';
 
 export class UserChatRoomValidator {
     static async validateUserAndChatRoom(userID: Types.ObjectId, chatRoomID: Types.ObjectId) {
         const [isUserValid, isChatRoomValid] = await Promise.all([
-            checkUserIdExists(userID.toString()),
+            checkExistsInUser("userId", userID.toString()),
             checkChatRoomIdExists(chatRoomID.toString())
         ]);
 
