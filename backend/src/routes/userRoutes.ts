dfe78775ec1bdab57d96cf4ba2a05911813c6787
natylr <51606937+natylr@users.noreply.github.com>
2024-09-25 18:21:09 +0300

    feat: apply checkNotDuplicateEmailPhoneMiddleware to user creation route

--- backend/src/routes/userRoutes.ts
+++ backend/src/routes/userRoutes.ts
@@ -2,9 +2,10 @@ import { Router } from 'express';
 import { createUser, getAllUsers, getUserById, updateUserById, deleteUserById, isUsernameAvailable, loginUser } from '../controllers/userController';
 import { isUsernameAvailableMiddleware } from '../middleware/user/isUsernameAvailableMiddleware';
 import verifyJWTMiddleware from '../middleware/auth/verifyJWTverifyJWTMiddleware ';
+import { checkNotDuplicateEmailPhoneMiddleware } from '../middleware/user/checkNotDuplicateEmailPhoneMiddleware';
 const router = Router();
 
-router.post('/users', isUsernameAvailableMiddleware, createUser);
+router.post('/users',checkNotDuplicateEmailPhoneMiddleware, isUsernameAvailableMiddleware, createUser);
 router.get('/users',verifyJWTMiddleware, getAllUsers);
 router.get('/users/:id',verifyJWTMiddleware, getUserById);
 router.put('/users/:id',verifyJWTMiddleware, updateUserById);
