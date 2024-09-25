0a8d3d666f397b345229bd2560f146b94284c184
natylr <51606937+natylr@users.noreply.github.com>
2024-09-25 15:22:03 +0300

    remove: remove checkUserIdExists and checkUsernameExists modules,  as it's no longer needed since the checkExistsInUser function can handle both

--- backend/src/utils/validation/checkUserIdExists.ts
+++
@@ -1,6 +0,0 @@
-import { User } from '../../models/user';
-
-export const checkUserIdExists = async (userId: string): Promise<boolean> => {
-    const user = await User.findById(userId);
-  return !!user;
-};
--- backend/src/utils/validation/checkUsernameExists.ts
+++
@@ -1,6 +0,0 @@
-import { User } from '../../models/user';
-
-export const checkUsernameExists = async (username: string): Promise<boolean> => {
-  const user = await User.findOne({ username });
-  return !!user;
-};
