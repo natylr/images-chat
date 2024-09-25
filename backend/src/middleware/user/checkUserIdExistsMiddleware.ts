4d5ffedd12871b1a43af986b49b7949e35ede5ca
natylr <51606937+natylr@users.noreply.github.com>
2024-09-25 12:42:23 +0300

    Refactor: Replace checkUserIdExists with checkExistsInUser for improved flexibility
    - Updated the middleware to use the `checkExistsInUser` function for more generic field checks.
    - Added a `field` parameter to the `checkExistsInUser` function to allow checking for different fields within the \"user\" collection.
    - Refined the error message to provide more context about the invalid UserID.

--- backend/src/middleware/user/checkUserIdExistsMiddleware.ts
+++ backend/src/middleware/user/checkUserIdExistsMiddleware.ts
@@ -1,26 +1,26 @@
 import { Request, Response, NextFunction } from 'express';
-import { checkUserIdExists } from '../../utils/validation/checkUserIdExists';
 import mongoose from 'mongoose';
+import { checkExistsInUser } from '../../utils/validation/checkExistsInUser';
 
 export const checkUserIdExistsMiddleware = async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    const userId = req.params.userId || req.body.userId || req.query.userId; // Extract userId from request
+    try {
+        const userId = req.params.userId || req.body.userId || req.query.userId;
 
-    if (!userId) {
-      return res.status(400).json({ error: 'UserID is required' });
+        if (!userId) {
+            return res.status(400).json({ error: 'UserID is required' });
+        }
+
+        const user = await checkExistsInUser("userId", userId.toString());
+
+        if (!user) {
+            return res.status(404).json({ error: 'Invalid User Association: The provided UserID does not exist in our database.' });
+        }
+
+        next();
+    } catch (error) {
+        if (error instanceof mongoose.Error) {
+            return res.status(500).json({ error: 'Database error occurred while checking UserID existence.' });
+        }
+        next(error);
     }
-
-    const user = await checkUserIdExists(userId);
-
-    if (!user) {
-      return res.status(404).json({ error: 'Invalid User Association: User with the provided userID does not exist' });
-    }
-
-    next(); // User exists, proceed to the next middleware or route handler
-  } catch (error) {
-    if (error instanceof mongoose.Error) {
-      return res.status(500).json({ error: 'Database error' });
-    }
-    next(error); // Pass error to the error handling middleware
-  }
 };
