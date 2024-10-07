fa5ad14366823040c4ca8e1e5cf32c9f17cc4d07
natylr <51606937+natylr@users.noreply.github.com>
2024-10-07 22:20:05 +0300

    Enforce userId existence before creating/updating user roles using checkUserIdExistsMiddleware on POST/PUT /userroles routes

--- backend/src/routes/userRoleRoutes.ts
+++ backend/src/routes/userRoleRoutes.ts
@@ -1,13 +1,14 @@
 import { Router } from 'express';
 import { createUserRole, getAllUserRoles, getUserRoleById, updateUserRoleById, deleteUserRoleById } from '../controllers/userRoleController';
 import verifyJWTMiddleware from '../middleware/auth/verifyJWTverifyJWTMiddleware ';
+import { checkUserIdExistsMiddleware } from '../middleware/user/checkUserIdExistsMiddleware';
 
 const router: Router = Router();
 
-router.post('/userroles',verifyJWTMiddleware, createUserRole);
+router.post('/userroles',verifyJWTMiddleware,checkUserIdExistsMiddleware, createUserRole);
 router.get('/userroles',verifyJWTMiddleware, getAllUserRoles);
 router.get('/userroles/:id',verifyJWTMiddleware, getUserRoleById);
-router.put('/userroles/:id',verifyJWTMiddleware, updateUserRoleById);
+router.put('/userroles/:id',verifyJWTMiddleware,checkUserIdExistsMiddleware, updateUserRoleById);
 router.delete('/userroles/:id',verifyJWTMiddleware, deleteUserRoleById);
 
 export default router;
