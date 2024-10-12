d5d5f247ddc56d4364117c05ede480d39eb2705a
natylr <51606937+natylr@users.noreply.github.com>
2024-10-12 21:04:59 +0300

    feat: Refactor message middleware to update ChatRoom timestamp with populated chatRoomMember
    - Added `updateChatRoomTimestampFromMessage` function to populate `chatRoomMemberId` and update the ChatRoom timestamp.
    - Replaced direct calls to `updateChatRoomTimestamp` with `updateChatRoomTimestampFromMessage` in `post` hooks (`save`, `findOneAndUpdate`, `findOneAndDelete`).
    - Enhanced error handling in `updateChatRoomTimestampFromMessage` for better error tracking.

--- backend/src/middleware/message/messageMiddleware.ts
+++ backend/src/middleware/message/messageMiddleware.ts
@@ -1,25 +1,40 @@
 import { updateChatRoomTimestamp } from '../../utils/transformation/updateChatRoomTimestamp';
 import { Schema } from 'mongoose';
 import { IMessage } from '../../models/message';
+import { ChatRoomMember } from '../../models/chatRoomMember';
+
+async function updateChatRoomTimestampFromMessage(doc: IMessage) {
+  try {
+    await doc.populate('chatRoomMemberId');
+    const chatRoomMember = doc.chatRoomMemberId;
+    if (chatRoomMember instanceof ChatRoomMember) {
+      const chatRoomID = chatRoomMember.chatRoomID;
+      await updateChatRoomTimestamp(chatRoomID);
+    }
+  } catch (error) {
+    console.error('Error updating ChatRoom timestamp:', error);
+  }
+}
 
 export async function setupMessageMiddleware(schema: Schema) {
   const { checkUserAndChatRoomIdMiddleware: checkUserAndChatRoomId } = await import('./checkUserAndChatRoomIdMiddleware');
 
+  // Pre-save hook to check user and chat room IDs exists
   schema.pre('save', checkUserAndChatRoomId);
 
   schema.post('save', async function (doc: IMessage) {
-    await updateChatRoomTimestamp(doc.chatRoomID);
+    await updateChatRoomTimestampFromMessage(doc);
   });
 
   schema.post('findOneAndUpdate', async function (doc: IMessage | null) {
     if (doc) {
-      await updateChatRoomTimestamp(doc.chatRoomID);
+      await updateChatRoomTimestampFromMessage(doc);
     }
   });
 
   schema.post('findOneAndDelete', async function (doc: IMessage | null) {
     if (doc) {
-      await updateChatRoomTimestamp(doc.chatRoomID);
+      await updateChatRoomTimestampFromMessage(doc);
     }
   });
 }
