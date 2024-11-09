b9e664574d12b3110030bb1d6b315a4289c2275d
natylr <51606937+natylr@users.noreply.github.com>
2024-11-09 21:32:57 +0200

    feat: Add category validation middleware and update image routes
    - Created `checkCategoryExistsMiddleware` to validate `categoryId` existence in requests
    - Applied `checkCategoryExistsMiddleware` to POST and PUT routes in `imageRoutes`

---
+++ backend/src/middleware/category/checkCategoryIdExistsMiddleware.ts
@@ -0,0 +1,21 @@
+import { Request, Response, NextFunction } from "express";
+import { checkCategoryExists } from "../../utils/validation/checkCategoryExists";
+
+export const checkCategoryIdExistsMiddleware = async (req: Request, res: Response, next:NextFunction){
+    try{
+    const { categoryId } = req.body || req.params || req.query; 
+
+    if (!categoryId) {
+        return res.status(400).json({ error: 'categoryId is required' });
+    }
+
+    const exists = await checkCategoryExists(categoryId)
+    if(!exists){
+        return res.status(400).json({error: "Category with the provided ID does not exist'"});
+    }
+   next();
+}
+catch(error){
+    next(error);
+}
+}
--- backend/src/routes/imageRoutes.ts
+++ backend/src/routes/imageRoutes.ts
@@ -1,12 +1,13 @@
 import {Router} from 'express'
 import { createImage, getAllImages, getImageById, updateImageById, deleteImageById, getImagesByCategoryNameHandler } from '../controllers/imageController';
+import { checkCategoryIdExistsMiddleware } from '../middleware/category/checkCategoryIdExistsMiddleware';
 
 const router = Router();
 
-router.post('/images', createImage);
+router.post('/images',checkCategoryIdExistsMiddleware, createImage);
 router.get('/images', getAllImages);
 router.get('/images/:id', getImageById);
-router.put('/images/:id', updateImageById);
+router.put('/images/:id', checkCategoryIdExistsMiddleware, updateImageById);
 router.delete('/images/:id', deleteImageById);
 router.get('/images/category/:categoryName', getImagesByCategoryNameHandler);
 
