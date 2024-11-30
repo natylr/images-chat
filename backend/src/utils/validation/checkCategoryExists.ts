2d11360fc8bd6e03dc7d7a9454e2307919daaa08
natylr <51606937+natylr@users.noreply.github.com>
2024-11-30 21:49:06 +0200

    feat(category): Add type definition for categoryId in checkCategoryExists
    The `categoryId` parameter in the `checkCategoryExists` function was previously untyped.  This commit adds a type definition specifying that `categoryId` must be a string.  This improves type safety and code clarity.

--- backend/src/utils/validation/checkCategoryExists.ts
+++ backend/src/utils/validation/checkCategoryExists.ts
@@ -1,5 +1,5 @@
 import { Category } from "../../models/category";
 
-export const checkCategoryExists = async (categoryId) => {
+export const checkCategoryExists = async (categoryId: string) => {
   return await Category.exists({ _id: categoryId });
 };
