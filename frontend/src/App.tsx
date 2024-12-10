cfb59ccbaa212f45ef1d0b79f002e173f425cc38
natylr <51606937+natylr@users.noreply.github.com>
2024-12-10 20:37:34 +0200

    Refactored App.tsx to include AuthProvider for authentication context

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,24 +5,27 @@ import Home from './pages/Home';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import './styles/App.css';
+import { AuthProvider } from './context/AuthContext';
 
 const App: React.FC = () => {
   return (
     <div className="app-container">
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/register" element={<RegisterPage />} />
-        </Routes>
-      </Router>
-      
-      {/* Wave background effect */}
-      <div className="wave"></div>
-      <div className="wave"></div>
-      <div className="wave"></div>
-    </div>
-  );
+      <AuthProvider>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/register" element={<RegisterPage />} />
+          </Routes>
+        </Router>
+        </AuthProvider>
+
+          {/* Wave background effect */}
+          <div className="wave"></div>
+          <div className="wave"></div>
+          <div className="wave"></div>
+        </div>
+        );
 };
 
-export default App;
+        export default App;
