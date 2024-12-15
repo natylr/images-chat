f50d57dd232770dbb472e06c2db22302e1d004ee
natylr <51606937+natylr@users.noreply.github.com>
2024-12-15 19:32:10 +0200

    Refactored `verifyUserOwnsResource` to use `req.user._id` instead of `req.userId'

--- backend/src/middleware/auth/verifyUserOwnsResource.ts
+++ backend/src/middleware/auth/verifyUserOwnsResource.ts
@@ -3,8 +3,8 @@ import { Types } from 'mongoose';
 
 const verifyUserOwnsResource = (req: IUserIdRequest): boolean => {
   if ("body" in req && "userID" in req.body){
-    const userIdFromToken = req.userId; // This is set by the verifyJWT middleware
-    const userIdFromBody = req.body.userID; // This is the userID in the request body
+    const userIdFromToken = req.user?._id; // This is set by the verifyJWT middleware
+    const userIdFromBody = req.body.userId; // This is the userID in the request body
     return userIdFromToken === userIdFromBody;
   }
   else{
